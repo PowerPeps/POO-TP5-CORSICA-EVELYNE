82c4b05f6f2d99f7cf37e6e97599ddce2ef49cce
Emile Tôlerie <77052948+PowerPeps@users.noreply.github.com>
2024-11-27 22:00:47 +0100

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,6 +1,6 @@
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Course {
     private String titre;
@@ -13,6 +13,14 @@ public class Course {
         this.gagnants = new ArrayList<>();
     }
 
+    public List<Bateau> getParticipants() {
+        return participants;
+    }
+
+    public List<Bateau> getGagnants() {
+        return gagnants;
+    }
+
     public void inscrireBateau(Bateau b, Equipage e) {
         b.affecterEquipage(e);
         participants.add(b);
@@ -20,23 +28,39 @@ public class Course {
 
     public void trouverGagnants() {
         double tempsMin = participants.stream()
-                                       .mapToDouble(Bateau::getTempsPondere)
-                                       .min()
-                                       .orElse(Double.MAX_VALUE);
-        for (Bateau b : participants) {
-            if (b.getTempsPondere() == tempsMin) {
-                gagnants.add(b);
-            }
-        }
+                                      .mapToDouble(Bateau::getTempsPondere)
+                                      .min()
+                                      .orElse(Double.MAX_VALUE);
+
+        gagnants = participants.stream()
+                               .filter(b -> b.getTempsPondere() == tempsMin)
+                               .collect(Collectors.toList());
     }
 
     public void afficherGagnants() {
-        if (gagnants.size() == 1) {
+        if (gagnants.isEmpty()) {
+            System.out.println("Aucun gagnant n'a été déterminé.");
+        } else if (gagnants.size() == 1) {
             System.out.println("Le grand gagnant est " + gagnants.get(0).getNom());
         } else {
             System.out.print("Il y a " + gagnants.size() + " gagnants ex-aequo : ");
             gagnants.forEach(b -> System.out.print(b.getNom() + " "));
+            System.out.println();
         }
-        System.out.println();
+    }
+
+    public void afficherResultats() {
+        if (participants.isEmpty()) {
+            System.out.println("Aucun résultat à afficher, aucun participant.");
+        } else {
+            System.out.println("Résultats de la course " + this.titre + " :");
+            for (Bateau b : participants) {
+                String resultat = "Le bateau " + b.getNom() +
+                                  " de " + b.getEquipage().getCapitaine().getNom() +
+                                  " (barreur) a réalisé un temps pondéré de " +
+                                  String.format("%.2f", b.getTempsPondere()) + " mn.";
+                System.out.println(resultat);
+            }
+        }
     }
 }
